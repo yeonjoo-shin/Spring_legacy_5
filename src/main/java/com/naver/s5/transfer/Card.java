3ee4418e61b4d3b7accd908a63c3c5c46d98b7d6
yeonjoo-shin <hm02138@naver.com>
2020-05-07 12:38:23 +0900

    comm

--- src/main/java/com/naver/s5/transfer/Card.java
+++ src/main/java/com/naver/s5/transfer/Card.java
@@ -1,15 +1,22 @@
 package com.naver.s5.transfer;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class Card {
-	
+	@AfterReturning("execution(* com.naver.s5.transfer.Transfer.*())")
 	public void cardAfter() {
 		System.out.println("----------------");
 		System.out.println("카드 결제");
 	}
 	
-
+	@Around("execution(* com.naver.s5.transfer.Transfer.*(Integer, ..))")
 	public Object cardCheck(ProceedingJoinPoint join) throws Throwable {
 		System.out.println("---------");
 		
